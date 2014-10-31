33881adffeb5277874fb3a4a500479f04e91506e
machiko1016 <hamachi1016@gmail.com>
2014-10-31 12:56:31 +0900

    complite (Rikoten) 2

--- src/net/exkazuu/ManekkoDance/Help.java
+++ src/net/exkazuu/ManekkoDance/Help.java
@@ -11,10 +11,10 @@ import android.widget.ImageView;
 public class Help extends Activity {
 
 	private int pageNumber;
-	private final String page = " / 6";
+	private final String page = " / 7";
 
 	int[] helpImageResources = { R.drawable.tutorial1, R.drawable.tutorial2,
-			R.drawable.tutorial3, R.drawable.tutorial4, 
+			R.drawable.tutorial3, R.drawable.tutorial4, R.drawable.tutorial5,
 			R.drawable.helptext1, R.drawable.helptext2 };
 
 	public void onCreate(Bundle savedInstanceState) {
@@ -25,11 +25,11 @@ public class Help extends Activity {
 		ImageView helpImage = (ImageView) findViewById(R.id.helpImage);
 		helpImage.setImageResource(R.drawable.tutorial1);
 		EditText pageText = (EditText) findViewById(R.id.page);
-		pageText.setText("1 / 6");
+		pageText.setText("1 / 7");
 	}
 
 	public void next(View view) {
-		if (pageNumber == 6)
+		if (pageNumber == 7)
 			pageNumber = 0;
 		pageNumber++;
 		setHelpImage();
@@ -37,7 +37,7 @@ public class Help extends Activity {
 
 	public void prev(View view) {
 		if (pageNumber == 1)
-			pageNumber = 6;
+			pageNumber = 7;
 		pageNumber--;
 		setHelpImage();
 	}
