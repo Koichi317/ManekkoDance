bc42df36cf614e4f2367627f189108985f7ce3e9
Sakamoto, Kazunori <exkazuu@gmail.com>
2015-08-01 01:42:41 +0900

    Fix PreQuestionnaireResult model.

---
+++ app/src/main/java/net/exkazuu/mimicdance/models/PreQuestionnaireResult.java
@@ -0,0 +1,36 @@
+package net.exkazuu.mimicdance.models;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+@Table(name = "PreQuestionnaireResult")
+public class PreQuestionnaireResult extends Model {
+    @Column(name = "Id")
+    public String id;
+
+    @Column(name = "Age")
+    public int age;
+
+    @Column(name = "Interest")
+    public int interest;
+
+    @Column(name = "Fun")
+    public int fun;
+
+    @Column(name = "Feasibility")
+    public int feasibility;
+
+    @Column(name = "Usefulness")
+    public int usefulness;
+
+    @Column(name = "Sex")
+    public String sex;
+
+    @Column(name = "KnowledgeOfProgramming")
+    public String knowledgeOfProgramming;
+
+    @Column(name = "KnowledgeOfMimicDance")
+    public String knowledgeOfMimicDance;
+}
+
